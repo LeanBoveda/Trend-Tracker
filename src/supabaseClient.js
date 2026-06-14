476bc3512db834f38296e4e326cf72f0ba4d0cdc
leanboveda <leandroboveda@gmail.com>
2026-06-14 12:57:06 -0300

    supabase update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Estas variables las obtendremos del panel de Supabase en un momento
-const supabaseUrl = 'AQUI_IRA_TU_URL';
-const supabaseAnonKey = 'AQUI_IRA_TU_CLAVE';
+const supabaseUrl = 'sb_publishable_kqzR96TsA774Z4FY5M4sNA_RpkK_9Wu';
+const supabaseAnonKey = 'https://owpbxwucijuqrwumltoq.supabase.co';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
